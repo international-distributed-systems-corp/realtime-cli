c4072afdd98cf3b6a43b4df974b05af0d13c0376
Arthur Collé <arthur@collegroup.com>
2025-01-28 17:53:23 -0500

    feat: Create initial App.js for digital twin application

---
+++ digital-twin-app/src/App.js
@@ -0,0 +1,11 @@
+import DigitalTwinInterface from './components/DigitalTwinInterface';
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-50">
+      <DigitalTwinInterface />
+    </div>
+  );
+}
+
+export default App;
